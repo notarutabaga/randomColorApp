a54bed3a482d5d65dbb146325a89f3dc851e42d0
notarutabaga <abriannalynn0027@gmail.com>
2022-04-08 00:05:22 -0400

    initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,7 @@
+/*
+* this is where everything that happens in the gui goes
+* this class is gonna get very full so you can make another class for custom classes and stuff
+*/
+
+public class Controller {
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+* alt enter on Application and import the javafx one
+* alt enter Main line and implement methods keeping default options
+* alt enter @Override and select add modules option
+*
+* have to alt enter to fix a lot
+* always choose to import the javafx one if given an option
+*/
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        primaryStage.setTitle("GUI");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show(); // this always comes at the end of this method
+    }
+}
